2c06fb2e5333afa63e0b4b5341ff52827507369e
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2025-11-29 00:07:15 +0700

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -24,9 +24,14 @@ ALLOWED_HOSTS = ["*", ".cognitiveclass.ai", "localhost", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.cognitiveclass.ai",
-    "https://*.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
-    # Sửa E501: Dòng quá dài
-    "https://minhnlnce181-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    (
+        "https://*.theianext-1-labs-prod-misc-tools-us-east-0"
+        ".proxy.cognitiveclass.ai"
+    ),
+    (
+        "https://minhnlnce181-8000.theianext-1-labs-prod-misc-tools-us-east-0"
+        ".proxy.cognitiveclass.ai"
+    ),
 ]
 
 
