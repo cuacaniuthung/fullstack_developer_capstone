dee0d8964b6c8036fab964632d5bb5b0c67175fd
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2025-11-28 22:50:55 +0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-
     def __str__(self):
         return self.name 
         # Return the name as the string representation
-
 class CarModel(models.Model):
     # Many-to-One relationship. Ngắt dòng để tránh lỗi E501.
     car_make = models.ForeignKey(
@@ -37,4 +34,5 @@ class CarModel(models.Model):
     # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name  
+        # Return the name as the string representation
