5a0f1a737083c0a20b1dab14d292910c95b1486d
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2025-11-28 22:42:44 +0700

    Update urls.py
    Removed unnecessary comments from the URL configuration.

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,8 +1,3 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
