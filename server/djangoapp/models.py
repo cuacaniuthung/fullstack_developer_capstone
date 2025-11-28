f19b33aaffc345c08b1e99d9034967ef6e862de7
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2025-11-28 21:51:07 +0700

    Refactor CarModel fields for better readability

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,10 +9,13 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name 
         # Return the name as the string representation
-        
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
-    # Many-to-One relationship
+    # Many-to-One relationship. Ngắt dòng để tránh lỗi E501.
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -24,13 +27,14 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
     # Other fields as needed
 
     def __str__(self):
-        return self.name  
-        # Return the name as the string representation
+        return self.name  # Return the name as the string representation
