1148f966ca798b17e08dec344035818fbb3dfbdd
Nguyen_Le_Ngoc_Minh <122802717+cuacaniuthung@users.noreply.github.com>
2025-11-28 22:21:18 +0700

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -2,16 +2,6 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -21,16 +11,25 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Chỉ giữ một lần include djangoapp.urls
     path('djangoapp/', include('djangoapp.urls')),
+
+    # Định nghĩa các route trả về HTML/React
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
-    path('djangoapp/', include('djangoapp.urls')),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
+    # Đường dẫn cho Dealer Detail và Review (chuyển hướng sang index.html để React xử lý)
+    # Sửa lỗi E225: thêm khoảng trắng quanh dấu phẩy và toán tử
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
